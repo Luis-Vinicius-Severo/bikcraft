a310be4e87331cd6c29a6422db6f3ae809741c05
luislucasinter14@hotmail.com <luislucasinter14@hotmail.com>
2024-10-24 11:35:29 -0300

    Galeria imagens

--- js/script.js
+++ js/script.js
@@ -37,9 +37,25 @@ function ativarPergunta(event) {
   const ativo = resposta.classList.contains('ativo')
   pergunta.setAttribute('aria-expanded', ativo)
 }
-
 function eventosPerguntas(pergunta) {
   pergunta.addEventListener('click', ativarPergunta)
 }
 
 perguntas.forEach(eventosPerguntas)
+
+//Galeria de bicicletas
+
+const galeria = document.querySelectorAll('.bicicleta-imagens img')
+const galeriaContainer = document.querySelector('.bicicleta-imagens')
+
+function trocarImagem(event) {
+  const img = event.currentTarget
+  const media = matchMedia('(min-width: 1000px)').matches
+  if (media) {
+    galeriaContainer.prepend(img)
+  }
+}
+function EventosGaleria(img) {
+  img.addEventListener('click', trocarImagem)
+}
+galeria.forEach(EventosGaleria)
